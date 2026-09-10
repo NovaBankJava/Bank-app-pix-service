a2ae11ba7f7d0b531ca9a00f61641637a83e90f5
G. Sales <gustavosales0809@gmail.com>
2026-09-10 14:13:51 -0300

    first commit

---
+++ src/main/java/org/example/bankapppixservice/BankAppPixServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankapppixservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppPixServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAppPixServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bankapppixservice/BankAppPixServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankapppixservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppPixServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
